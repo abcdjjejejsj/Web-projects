32135d3f9e68b8aec22653b68cccd63e2656cf8a
Vaibhav Salve <144205049+abcdjjejejsj@users.noreply.github.com>
2025-04-12 14:39:12 +0530

    Create index.js

---
+++ 03_Calculator/index.js
@@ -0,0 +1,49 @@
+let buttons = document.querySelectorAll("button");
+    let a="";
+    let as="";
+    let b=document.getElementById("ex");
+    let res=0;
+    let e=document.getElementById("answer");
+
+buttons.forEach(function(button){
+    button.addEventListener("click", function(){
+        // your logic here
+        let d=button.innerHTML;
+        if(d=="DEL")
+    {
+        a=a.slice(0,-1);
+        as=as.slice(0,-1);
+        b.innerHTML=as;
+    }
+        if(d=="x")
+    {
+        a+="*";
+        as+=d
+        b.innerHTML=as;  
+    }
+        if(d!="AC" && d!="=" && d!="x" && d!="DEL")
+    {
+        a+=d;
+        as+=d;
+        b.innerHTML=as;        
+    }
+    if(d=="AC")
+    {
+        a=""; 
+        as="";
+        b.innerHTML="0";  
+        e.innerHTML="0";
+           
+    }    
+    });
+});
+
+document.getElementById("ans").addEventListener("click",function(){
+   try{
+    res=eval(a);
+   }catch{
+    res="Error";
+   }
+e.innerHTML=res;
+    
+})
